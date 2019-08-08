0a78cc715bccb1c21a7f285640a7a7246a69526b
Travis Watson <watsonjamest@gmail.com>
2019-08-08 13:58:49 -0500

    Final commit

--- collatz.rb
+++ collatz.rb
@@ -1,18 +1,32 @@
 
-def collatz(n)
-    sum = n
+def collatz(input)
     sequence_length = 1
 
-    while sum != 1 do
-        if sum.even?
-            sum = sum/2
+    while input != 1 do
+        if input.even?
+            input = input/2
             sequence_length += 1
         else
-            sum = 3 * sum + 1
+            input = 3 * input + 1
             sequence_length += 1
         end
     end
-    puts "The sequence length is: " + sequence_length.to_s
+    sequence_length
 end
 
-collatz(7)
+def maximum
+    max_length = 0
+    max_number = 0
+
+    (1..1000000).each do |n|
+        length = collatz(n).to_i
+        if length > max_length
+            max_length = length
+            max_number = n
+        end
+    end
+    puts max_number
+end
+
+maximum
+#collatz(7)
